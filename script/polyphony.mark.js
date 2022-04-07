4c79e7208f53a5df0ecdec4770973e92a2b27c85
Acgnu <1814837440@qq.com>
2022-04-07 17:51:27 +0800

    polyphony store

--- script/polyphony.mark.js
+++ script/polyphony.mark.js
@@ -4,6 +4,7 @@ var footageIndex = 1
 var activeKeyList = []
 //所有复音集合
 var keyFootages = {}
+var storageKey = 'polyphony'
 
 $(function () {
   //禁用浏览器默认右键菜单
@@ -23,6 +24,9 @@ $(function () {
     }
   }
 
+  //从存储中恢复复音
+  recoverPolyphony()
+
   //复音创建事件
   $('#button_create_footage').click(function(e){
     if(activeKeyList.length == 0) return
@@ -37,6 +41,7 @@ $(function () {
       activeKeyCopy.push(activeKeyList[i])
     }
     keyFootages[footageIndex++] = activeKeyCopy
+    savePolyphony()
   })
 
   //绑定复音点击事件
@@ -68,6 +73,7 @@ $(function () {
     }
     _me.parent().remove()
     delete keyFootages[e.target.parentElement.getAttribute('footage-index')]
+    removePolyphony(e.target.parentElement.getAttribute('footage-index') - 1)
   });
 
   //绑定移除所有复音按钮点击事件
@@ -76,6 +82,7 @@ $(function () {
     deactiveAllKey()
     footageIndex = 1
     keyFootages = {}
+    removePolyphony()
   })
 })
 
@@ -87,11 +94,95 @@ function deactiveAllKey(){
   activeKeyList = []
 }
 
+/**
+ * 线点击事件
+ * @param {} keyDom 
+ * @returns 
+ */
 function onLineTriggerClick(keyDom) {
   if(keyDom){
     if($(keyDom).hasClass('key_highlight_fix')) return
     $(keyDom).addClass('key_highlight_fix')
     activeKeyList.push(keyDom)
     playByKey(getKeyNumByKeyDom(keyDom))
+  }
+}
+
+/**
+ * 恢复所有复音组
+ */
+function recoverPolyphony() {
+  let p = localStorage.getItem(storageKey)
+  if(p) {
+    let pObj = JSON.parse(p)
+    for(let o in pObj) {
+      let pkeys = pObj[o]
+      let activeKeyDoms = []
+      for(let ki in pkeys) {
+        let keyIdxInfo = pkeys[ki]
+        let keyGroupDom = $('.key_group[group-index=' + keyIdxInfo.gi + ']')
+        let keyDom = keyGroupDom.children()[keyIdxInfo.ki]
+        activeKeyDoms.push(keyDom)
+      }
+      let dom = '<div class="footage_item" footage-index="' + footageIndex + '">'
+      dom += '<div class="footage_remove">-</div>'
+      dom += '<p>复音' + footageIndex + '</p>'
+      dom += '</div>'
+      $('#footage_list').append(dom)
+      keyFootages[footageIndex++] = activeKeyDoms
+    }
+  }
+}
+
+/**
+ * 删除复音存储, 可指定下标
+ * @param {*} idx 需要删除的下标, 不指定删除全部
+ * @returns 
+ */
+function removePolyphony(idx) {
+  if(typeof idx != 'undefined') {
+    let ds = localStorage.getItem(storageKey)
+    if(!ds) return
+    let data = JSON.parse(ds)
+    if(data && data.length > idx) {
+      data.splice(idx,1); 
+      localStorage.setItem(storageKey, JSON.stringify(data))
+    }
+  } else {
+    localStorage.removeItem(storageKey)
+  }
+}
+
+/**
+ * 保存复音
+ */
+function savePolyphony() {
+  //复音总对象, 转换成数组
+  let data = []
+  for(let idx in keyFootages) {
+    let keymap = keyFootages[idx]
+    let subData = []
+    let subKey = {}
+    for(let ki in keymap) {
+      let keyGroupIndex = keymap[ki].parentElement.getAttribute('group-index')
+      let keyIndex = 0
+      let pChild = keymap[ki].parentElement.children
+      for(var i = 0; i< pChild.length; i++){
+        if(pChild[i] == keymap[ki]){
+          keyIndex = i;
+          break;
+        }
+      }
+      //保存键位组下标和键位下标
+      subKey = {
+        gi:parseInt(keyGroupIndex),
+        ki:keyIndex
+      }
+      subData.push(subKey)
+    }
+    data.push(subData)
+  }
+  if(data.length > 0) {
+    localStorage.setItem(storageKey, JSON.stringify(data))
   }
 }
